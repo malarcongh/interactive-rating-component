60b525f5ec88fbabb9043eaba032955bfbe8454f
malarcongh <malarconmu@gmail.com>
2023-07-10 17:25:35 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import RatingCard from "./components/RatingCard";
+
+function App() {
+  return (
+    <div className="rating-section">
+      <RatingCard />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/RatingCard.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef, useState } from 'react'
+import StarIcon from '../assets/icon-star.svg';
+import Illustration from '../assets/illustration-thank-you.svg';
+
+
+export default function RatingCard() {
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [score, setScore] = useState(null);
+  const [activeScoreRef, setActiveScoreRef] = useState(null);
+  const cardContainerRef = useRef(null);
+  const cardContainerHeight = isSubmitted ? cardContainerRef.current.offsetHeight: 'unset';
+
+
+  useEffect(() => {
+    if(!activeScoreRef) return;
+    activeScoreRef.classList.add('selected');
+  }, [activeScoreRef])
+
+  function onScoreClickHandler(e){
+    if(!e.target.closest('.score-item')) return;
+    setScore(e.target.innerText);
+    setActiveScoreRef(prev => {
+      if(prev) prev.classList.remove('selected');
+      return e.target;
+    })
+  }
+
+  function onSubmitHandlder(){
+    if(score === null) return;
+    setIsSubmitted(true)
+  }
+
+
+  return (
+    <div className="card-container" style={{height: cardContainerHeight}} ref={cardContainerRef}>
+      {!isSubmitted ? 
+        <div className="rating-card">
+          <div className="icon-container">
+            <img src={StarIcon} alt="Rating card" />
+          </div>
+          <h1 className="card-title">How did we do?</h1>
+          <p className="card-text">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+          <div className="score-container" onClick={onScoreClickHandler}>
+            <div>
+              <span className='score-item'>1</span>
+            </div>
+            <div>
+              <span className="score-item">2</span>
+            </div>
+            <div>
+              <span className="score-item">3</span>
+            </div>
+            <div>
+              <span className="score-item">4</span>
+            </div>
+            <div>
+              <span className="score-item">5</span>
+            </div>
+          </div>
+          <button className="submit-btn" onClick={onSubmitHandlder}>Submit</button>
+        </div>
+      : 
+        <div className='submitted-card'>
+          <div className="illustration-container">
+            <img src={Illustration} alt="" />
+          </div>
+          <div className="submitted-score-container">
+            {`You selected ${score} out of 5`}
+          </div>
+          <h1 className="submitted-card-title">Thank you!</h1>
+          <p className="submitted-card-text">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+        </div>
+      }
+      
+    </div>
+  )
+}
